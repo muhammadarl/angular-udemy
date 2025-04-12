9150d73510885b77364661d171bd4da72bd9faeb
muhammadarl <syiarul45@gmail.com>
2025-04-12 14:03:53 +0700

    Creating & Using Shared Modules

--- Modules-3/src/app/app.module.ts
+++ Modules-3/src/app/app.module.ts
@@ -4,14 +4,14 @@ import { UserComponent } from './user/user.component';
 import { TasksComponent } from "./tasks/tasks.component";
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { CardComponent } from './shared/card/card.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { TaskComponent } from './tasks/task/task.component';
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-    declarations: [AppComponent, UserComponent, CardComponent, HeaderComponent, TasksComponent, NewTaskComponent, TaskComponent],
-    imports: [BrowserModule, FormsModule],
+    declarations: [AppComponent, UserComponent, HeaderComponent, TasksComponent, NewTaskComponent, TaskComponent],
+    imports: [BrowserModule, FormsModule, SharedModule],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
