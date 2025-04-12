733bb099f94d6efc148a155067f1d902555d6a9f
muhammadarl <syiarul45@gmail.com>
2025-04-12 13:52:08 +0700

    Declaring & Using Components

--- Modules-3/src/app/app.module.ts
+++ Modules-3/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from "./tasks/tasks.component";
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [HeaderComponent, UserComponent, TasksComponent],
+    imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
