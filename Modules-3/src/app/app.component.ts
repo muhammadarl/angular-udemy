c30f5d39f3c765b9f1aec21b95ad8c56c2486a5e
muhammadarl <syiarul45@gmail.com>
2025-04-12 13:44:28 +0700

    Creating a First Empty Module

--- Modules-3/src/app/app.component.ts
+++ Modules-3/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
@@ -7,10 +6,9 @@ import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
     selector: 'app-root',
-    standalone: true,
+    standalone: false,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [HeaderComponent, UserComponent, TasksComponent]
 })
 export class AppComponent {
   users = DUMMY_USERS;
