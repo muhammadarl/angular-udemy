c776422c791e70d7cb80ee25da0e7e3649dbe3be
muhammadarl <syiarul45@gmail.com>
2025-04-12 23:35:24 +0700

    Calculating the Annual Investment Data

--- Modules-4/src/app/app.component.ts
+++ Modules-4/src/app/app.component.ts
@@ -8,4 +8,34 @@ import { UserInputComponent } from "./user-input/user-input.component";
   imports: [HeaderComponent, UserInputComponent],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+
+  calculateInvestmentResults(investParam:{
+    initialInvestment: number,
+    annualInvestment: number,
+    expectedReturn: number,
+    duration: number,
+  }) {
+    const annualData = [];
+    let investmentValue = investParam.initialInvestment;
+
+    for (let i = 0; i < investParam.duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (investParam.expectedReturn / 100);
+      investmentValue += interestEarnedInYear + investParam.annualInvestment;
+      const totalInterest =
+        investmentValue - investParam.annualInvestment * year - investParam.initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: investParam.annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: investParam.initialInvestment + investParam.annualInvestment * year,
+      });
+    }
+
+    return annualData;
+  }
+
+}
