264ed7a0db3e25b2d603bf6755a34a508110546d
muhammadarl <syiarul45@gmail.com>
2025-04-13 00:19:53 +0700

    Passing Data from Parent to Child with Inputs

--- Modules-4/src/app/app.component.ts
+++ Modules-4/src/app/app.component.ts
@@ -2,16 +2,26 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from "./user-input/user-input.component";
 import { InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  results:{
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
 
+  }[] = [];
   calculateInvestmentResults(investParam: InvestmentInput) {
+    
     const annualData = [];
     let investmentValue = investParam.initialInvestment;
 
@@ -30,6 +40,7 @@ export class AppComponent {
         totalAmountInvested: investParam.initialInvestment + investParam.annualInvestment * year,
       });
     }
+    this.results = annualData;
   }
 
 }
---
+++ Modules-4/src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,21 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+  investmentResults= input.required<{
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[]>();
+
+
+}
